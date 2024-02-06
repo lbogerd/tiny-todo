cd93affe608c7b6a55d87ab5e00f11f4409c153c
Lucas Bogerd <lucas@youngones.works>
2024-02-07 00:15:35 +0100

    Refactor TodoView to handle checkbox state change

--- src/views/TodoView.ts
+++ src/views/TodoView.ts
@@ -1,16 +1,27 @@
 import * as vscode from "vscode"
-import { TodoProvider } from "../providers/TodoProvider"
+import { TodoProvider, TodoTreeItem } from "../providers/TodoProvider"
 
 export class TodoView {
 	private _todoProvider: TodoProvider | undefined = undefined
 	readonly todoProvider: TodoProvider | undefined = this._todoProvider
 
 	constructor(context: vscode.ExtensionContext) {
-		this._todoProvider = new TodoProvider()
+		const provider = new TodoProvider()
+		this._todoProvider = provider
 
 		const view = vscode.window.createTreeView("tinyTodoView", {
 			treeDataProvider: this._todoProvider,
 			showCollapseAll: true,
+		})
+
+		view.onDidChangeCheckboxState((e) => {
+			const test = e.items.map((item) => {
+				return {
+					lineNumber: item[0].todoItem.lineNumber,
+					isSelected: item[1],
+				}
+			})
+			console.log(test)
 		})
 
 		context.subscriptions.push(view)
